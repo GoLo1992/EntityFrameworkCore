aace540964df8fc8a0f259e35bd6018255c193d8
AndriySvyryd <Andriy.Svyryd@microsoft.com>
2018-10-05 17:26:54 -0700

    Add virtual

--- src/EFCore/Query/ExpressionVisitors/Internal/EagerLoadingExpressionVisitorFactory.cs
+++ src/EFCore/Query/ExpressionVisitors/Internal/EagerLoadingExpressionVisitorFactory.cs
@@ -13,7 +13,7 @@ namespace Microsoft.EntityFrameworkCore.Query.ExpressionVisitors.Internal
         ///     This API supports the Entity Framework Core infrastructure and is not intended to be used
         ///     directly from your code. This API may change or be removed in future releases.
         /// </summary>
-        public EagerLoadingExpressionVisitor Create(
+        public virtual EagerLoadingExpressionVisitor Create(
             QueryCompilationContext queryCompilationContext,
             IQuerySourceTracingExpressionVisitorFactory querySourceTracingExpressionVisitorFactory)
             => new EagerLoadingExpressionVisitor(queryCompilationContext, querySourceTracingExpressionVisitorFactory);
